4a5f6c72ca3340ca69bcec7961da151e25900844
Shemplo <shemplo@outlook.com>
2018-10-22 14:25:44 +0300

    DB reconnection after big pause

--- Software design/SD - 4. Tasks Service/src/main/java/ru/shemplo/tasks/db/DBConnection.java
+++ Software design/SD - 4. Tasks Service/src/main/java/ru/shemplo/tasks/db/DBConnection.java
@@ -5,6 +5,7 @@ import static java.sql.DriverManager.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLNonTransientConnectionException;
 
 import ru.shemplo.snowball.utils.db.DBType;
 
@@ -37,35 +38,55 @@ public class DBConnection implements AutoCloseable {
 	    return instance;
 	}
 	
-	private final Connection DB;
+	private final DBType type;
+	private Connection DB;
 	
 	DBConnection (final DBType type) throws SQLException {
-		String dbUrl = System.getProperty ("tasks.db.url");
-		if (dbUrl == null || dbUrl.length () == 0) {
-			String text = "Property `tasks.db.url` has invalid value (EMPTY)";
-			throw new IllegalStateException (text);
-		}
-		
-		String login    = System.getProperty ("tasks.db.login"),
-		       password = System.getProperty ("tasks.db.password");
-		
-		String url = String.join (":", "jdbc", type.TYPE, dbUrl);
-		if (login != null && password != null) {
-		    this.DB = getConnection (url, login, password);
-		} else {
-		    this.DB = getConnection (url);
-		}
+		this.type = type; this.DB = connect ();
 	}
 	
 	DBConnection () throws SQLException {
-        this.DB = null;
-    }
+	    this.type = null; this.DB = null;
+	}
+	
+	private final Connection connect () throws SQLException {
+	    String dbUrl = System.getProperty ("tasks.db.url");
+        if (dbUrl == null || dbUrl.length () == 0) {
+            String text = "Property `tasks.db.url` has invalid value (EMPTY)";
+            throw new IllegalStateException (text);
+        }
+        
+        String login    = System.getProperty ("tasks.db.login"),
+               password = System.getProperty ("tasks.db.password");
+        
+        String url = String.join (":", "jdbc", type.TYPE, dbUrl);
+        if (login != null && password != null) {
+            return getConnection (url, login, password);
+        }
+        
+        return getConnection (url);
+	}
+	
 	
 	public ResultSet execute (String query) throws SQLException {
+	    try {
+	        return DB.prepareStatement (query).executeQuery ();
+	    } catch (SQLNonTransientConnectionException sqlntce) {
+	        // Try to reconnect to database
+	        this.DB = connect ();
+	    }
+	    
 	    return DB.prepareStatement (query).executeQuery ();
 	}
 	
 	public void update (String query) throws SQLException {
+	    try {
+	        DB.prepareStatement (query).execute ();
+        } catch (SQLNonTransientConnectionException sqlntce) {
+            // Try to reconnect to database
+            this.DB = connect ();
+        }
+	    
 		DB.prepareStatement (query).execute ();
 	}
 
