c2d8c03ca1d66fcba4afa8a7248e3fca97d6d1e4
Shemplo <shemplo@outlook.com>
2019-03-19 21:36:39 +0300

    Some GUI attempts

--- Software design/SD - 11. Fitness Events/fitness-administartion/src/main/java/ru/shemplo/fitness/administration/gfx/AdminController.java
+++ Software design/SD - 11. Fitness Events/fitness-administartion/src/main/java/ru/shemplo/fitness/administration/gfx/AdminController.java
@@ -1,16 +1,27 @@
 package ru.shemplo.fitness.administration.gfx;
 
+import java.io.IOException;
 import java.net.URL;
 
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.HBox;
 
 public class AdminController implements Initializable {
-
+    
+    //@FXML private BorderPane clientDetails;
+    @FXML private HBox clientDetails;
+    
     @Override
     public void initialize (URL location, ResourceBundle resources) {
-        
+        final URL url = getClass ().getResource ("/fxml/client.fxml");
+        try {
+            clientDetails.getChildren ().add (FXMLLoader.load (url));
+            //clientDetails.setRight (FXMLLoader.load (url));
+        } catch (IOException e) { e.printStackTrace(); }
     }
     
 }
